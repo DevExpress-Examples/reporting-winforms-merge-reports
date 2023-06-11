b9c82f581f61adecb1d1a2e677a8067f78675bda
DevExpressExampleBot <fakeEmail>
2023-06-11 20:28:43 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,84 +1,83 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace ReportMerging
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.button1 = New System.Windows.Forms.Button()
-			Me.button2 = New System.Windows.Forms.Button()
-			Me.button3 = New System.Windows.Forms.Button()
-			Me.SuspendLayout()
-			' 
-			' button1
-			' 
-			Me.button1.Location = New System.Drawing.Point(119, 33)
-			Me.button1.Name = "button1"
-			Me.button1.Size = New System.Drawing.Size(78, 31)
-			Me.button1.TabIndex = 0
-			Me.button1.Text = "1 + 2"
-			Me.button1.UseVisualStyleBackColor = True
-'			Me.button1.Click += New System.EventHandler(Me.button1_Click);
-			' 
-			' button2
-			' 
-			Me.button2.Location = New System.Drawing.Point(122, 97)
-			Me.button2.Name = "button2"
-			Me.button2.Size = New System.Drawing.Size(75, 38)
-			Me.button2.TabIndex = 1
-			Me.button2.Text = "1, 2"
-			Me.button2.UseVisualStyleBackColor = True
-'			Me.button2.Click += New System.EventHandler(Me.button2_Click);
-			' 
-			' button3
-			' 
-			Me.button3.Location = New System.Drawing.Point(122, 167)
-			Me.button3.Name = "button3"
-			Me.button3.Size = New System.Drawing.Size(75, 37)
-			Me.button3.TabIndex = 2
-			Me.button3.Text = "1, -1"
-			Me.button3.UseVisualStyleBackColor = True
-'			Me.button3.Click += New System.EventHandler(Me.button3_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(351, 258)
-			Me.Controls.Add(Me.button3)
-			Me.Controls.Add(Me.button2)
-			Me.Controls.Add(Me.button1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.button1 = New System.Windows.Forms.Button()
+            Me.button2 = New System.Windows.Forms.Button()
+            Me.button3 = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            ' 
+            ' button1
+            ' 
+            Me.button1.Location = New System.Drawing.Point(119, 33)
+            Me.button1.Name = "button1"
+            Me.button1.Size = New System.Drawing.Size(78, 31)
+            Me.button1.TabIndex = 0
+            Me.button1.Text = "1 + 2"
+            Me.button1.UseVisualStyleBackColor = True
+            AddHandler Me.button1.Click, New System.EventHandler(AddressOf Me.button1_Click)
+            ' 
+            ' button2
+            ' 
+            Me.button2.Location = New System.Drawing.Point(122, 97)
+            Me.button2.Name = "button2"
+            Me.button2.Size = New System.Drawing.Size(75, 38)
+            Me.button2.TabIndex = 1
+            Me.button2.Text = "1, 2"
+            Me.button2.UseVisualStyleBackColor = True
+            AddHandler Me.button2.Click, New System.EventHandler(AddressOf Me.button2_Click)
+            ' 
+            ' button3
+            ' 
+            Me.button3.Location = New System.Drawing.Point(122, 167)
+            Me.button3.Name = "button3"
+            Me.button3.Size = New System.Drawing.Size(75, 37)
+            Me.button3.TabIndex = 2
+            Me.button3.Text = "1, -1"
+            Me.button3.UseVisualStyleBackColor = True
+            AddHandler Me.button3.Click, New System.EventHandler(AddressOf Me.button3_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(351, 258)
+            Me.Controls.Add(Me.button3)
+            Me.Controls.Add(Me.button2)
+            Me.Controls.Add(Me.button1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents button1 As System.Windows.Forms.Button
-		Private WithEvents button2 As System.Windows.Forms.Button
-		Private WithEvents button3 As System.Windows.Forms.Button
-	End Class
+'#End Region
+        Private button1 As System.Windows.Forms.Button
+
+        Private button2 As System.Windows.Forms.Button
+
+        Private button3 As System.Windows.Forms.Button
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,90 +1,82 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraReports.UI
 
 Namespace ReportMerging
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			CombineTwoReports()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button2.Click
-			MergeTwoReports()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button3.Click
-			CreateBooklet()
-		End Sub
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            CombineTwoReports()
+        End Sub
 
-		Private Sub CombineTwoReports()
-			' Create the 1st report and generate its document.
-			Dim report1 As New XtraReport1()
-			report1.CreateDocument()
+        Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs)
+            MergeTwoReports()
+        End Sub
 
-			' Create the 2nd report and generate its document.
-			Dim report2 As New XtraReport2()
-			report2.CreateDocument()
+        Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs)
+            CreateBooklet()
+        End Sub
 
-			' Add all pages of the 2nd report to the end of the 1st report.
-			report1.Pages.AddRange(report2.Pages)
+        Private Sub CombineTwoReports()
+            ' Create the 1st report and generate its document.
+            Dim report1 As XtraReport1 = New XtraReport1()
+            report1.CreateDocument()
+            ' Create the 2nd report and generate its document.
+            Dim report2 As XtraReport2 = New XtraReport2()
+            report2.CreateDocument()
+            ' Add all pages of the 2nd report to the end of the 1st report.
+            report1.Pages.AddRange(report2.Pages)
+            ' Reset all page numbers in the resulting document.
+            report1.PrintingSystem.ContinuousPageNumbering = True
+            ' Show the Print Preview form.
+            report1.ShowPreviewDialog()
+        End Sub
 
-			' Reset all page numbers in the resulting document.
-			report1.PrintingSystem.ContinuousPageNumbering = True
+        Private Sub MergeTwoReports()
+            ' Create the 1st report and generate its document.
+            Dim report1 As XtraReport1 = New XtraReport1()
+            report1.CreateDocument()
+            ' Create the 2nd report and generate its document.
+            Dim report2 As XtraReport2 = New XtraReport2()
+            report2.CreateDocument()
+            ' Merge pages of two reports, page-by-page.
+            Dim minPageCount As Integer = Math.Min(report1.Pages.Count, report2.Pages.Count)
+            For i As Integer = 0 To minPageCount - 1
+                report1.Pages.Insert(i * 2 + 1, report2.Pages(i))
+            Next
 
-			' Show the Print Preview form.
-			report1.ShowPreviewDialog()
-		End Sub
+            If report2.Pages.Count <> minPageCount Then
+                For i As Integer = minPageCount To report2.Pages.Count - 1
+                    report1.Pages.Add(report2.Pages(i))
+                Next
+            End If
 
-		Private Sub MergeTwoReports()
-			' Create the 1st report and generate its document.
-			Dim report1 As New XtraReport1()
-			report1.CreateDocument()
+            ' Reset all page numbers in the resulting document.
+            report1.PrintingSystem.ContinuousPageNumbering = True
+            ' Show the Print Preview form.
+            report1.ShowPreviewDialog()
+        End Sub
 
-			' Create the 2nd report and generate its document.
-			Dim report2 As New XtraReport2()
-			report2.CreateDocument()
+        Private Sub CreateBooklet()
+            ' Create the 1st report and generate its document.
+            Dim report1 As XtraReport1 = New XtraReport1()
+            report1.CreateDocument()
+            ' Preserve original page numbers on all pages.
+            report1.PrintingSystem.ContinuousPageNumbering = False
+            ' Create a booklet.
+            Dim centerPageIndex As Integer = Convert.ToInt32((report1.Pages.Count - 1) \ 2)
+            For i As Integer = 0 To centerPageIndex - 1
+                report1.Pages.Insert(i * 2 + 1, report1.Pages(report1.Pages.Count - 1))
+            Next
 
-			' Merge pages of two reports, page-by-page.
-			Dim minPageCount As Integer = Math.Min(report1.Pages.Count, report2.Pages.Count)
-			For i As Integer = 0 To minPageCount - 1
-				report1.Pages.Insert(i * 2 + 1, report2.Pages(i))
-			Next i
-			If report2.Pages.Count <> minPageCount Then
-				For i As Integer = minPageCount To report2.Pages.Count - 1
-					report1.Pages.Add(report2.Pages(i))
-				Next i
-			End If
-
-			' Reset all page numbers in the resulting document.
-			report1.PrintingSystem.ContinuousPageNumbering = True
-
-			' Show the Print Preview form.
-			report1.ShowPreviewDialog()
-		End Sub
-
-		Private Sub CreateBooklet()
-			' Create the 1st report and generate its document.
-			Dim report1 As New XtraReport1()
-			report1.CreateDocument()
-
-			' Preserve original page numbers on all pages.
-			report1.PrintingSystem.ContinuousPageNumbering = False
-
-			' Create a booklet.
-			Dim centerPageIndex As Integer = Convert.ToInt32((report1.Pages.Count - 1) / 2)
-			For i As Integer = 0 To centerPageIndex - 1
-				report1.Pages.Insert(i * 2 + 1, report1.Pages(report1.Pages.Count - 1))
-			Next i
-
-			' Show the Print Preview form.
-			report1.ShowPreviewDialog()
-		End Sub
-	End Class
+            ' Show the Print Preview form.
+            report1.ShowPreviewDialog()
+        End Sub
+    End Class
 End Namespace
-
